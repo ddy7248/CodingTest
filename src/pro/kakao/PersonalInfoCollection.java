0faf1c9cf05a70ba5e119c6538126410d402c631
ddy <ddyy7248@gmail.com>
2023-01-19 12:25:21 +0900

    2023 KAKAO BLIND RECRUITMENT - 개인정보 수집 유효기간(완료)

--- src/pro/kakao/PersonalInfoCollection.java
+++ src/pro/kakao/PersonalInfoCollection.java
@@ -1,7 +1,13 @@
 package pro.kakao;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 public class PersonalInfoCollection {
@@ -20,15 +26,76 @@ public class PersonalInfoCollection {
 	}
 	
    public static int[] sortExpiredPersonalInfo(String today, String[] terms, String[] privacies) {
-        int[] resultTarget = {};
-        List<Integer> resultTargetList = new ArrayList<Integer>();
+	    int idx = 0;
+        int[] resultTarget = {};																// 약관 만료 대상(Array)
+        List<Integer> resultTargetList = new ArrayList<Integer>();								// 약관 만료 대상(List) 
+        HashMap<String, String> termsValidityMap = new HashMap<String, String>();				// 약관 종류:유효기간 매핑 정보 
+        LinkedHashMap<Integer, String> expiredDateMap = new LinkedHashMap<Integer, String>();	// 약관 만료 일자 매핑 정보
         
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
 
         // 약관 종류 - 유효기간 매핑
-        
+        for(String term : terms) {
+        	String[] termInfo = term.split(" ");
+        	String termType = "";		// 약관 종류
+        	String termValidity = "";	// 약관 유효기간(월)
+        	
+        	termType = termInfo[0];
+        	termValidity = termInfo[1];
+
+        	termsValidityMap.put(termType, termValidity);
+        }
         
         // 수집된 개인정보 유효기간 만료 확인
+        for (String privacy : privacies) {
+        	idx++;
+        	
+			String[] personalInfo = privacy.split(" ");
+			
+			String acceptDay = personalInfo[0];		// 약관 동의 일자(YYYY.MM.DD)
+			String termsType = personalInfo[1];		// 약관 종류
+			String expiredDay = "";					// 약관 만료 일자(YYYY.MM.DD)
+        	String termValidity = "";				// 약관 유효기간(월)
+
+			termValidity = termsValidityMap.get(termsType);
+			
+			try {
+				Date acceptDayDateFormat = sdf.parse(acceptDay);
+				
+				Calendar cal = Calendar.getInstance();
+				cal.setTime(acceptDayDateFormat);
+				cal.add(Calendar.MONTH, Integer.valueOf(termValidity));
+				cal.add(Calendar.DATE, -1);
+				
+				acceptDayDateFormat = cal.getTime();
+				if(acceptDayDateFormat.getDate() > 28) {
+					acceptDayDateFormat.setDate(28);
+				}		
+
+				expiredDay = sdf.format(acceptDayDateFormat);
+				
+			} catch(ParseException e) {
+				System.out.println("Parse error!!");
+			}
+			
+			expiredDateMap.put(idx, expiredDay);
+		}
         
+        // today 기준 만료 대상 추출
+        expiredDateMap.forEach((no, expiredDay) -> {
+        	try {
+        		Date todayDateFormat = sdf.parse(today);
+            	Date expiredDayDateFormat = sdf.parse(expiredDay);
+
+            	if(expiredDayDateFormat.compareTo(todayDateFormat) < 0) {
+            		resultTargetList.add(no);
+            	}
+            	
+        	} catch(ParseException e) {
+        		System.out.println("Parse Error!!");
+        	}
+        			
+        });
         
         // List<Integer> => int[]
         resultTarget = resultTargetList.stream()
